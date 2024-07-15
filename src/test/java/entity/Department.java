fff06c56c63dd93e1bdcc9c10268b86c4a5ebef5
taian@DESKTOP-JSAAH04 <taianhnguyen12@gmail.com>
2024-07-15 21:30:56 +0700

    create

--- src/test/java/entity/Department.java
+++ src/test/java/entity/Department.java
@@ -5,6 +5,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,4 +24,16 @@ public class Department {
     @Column(name = "name", length = 50, unique = true, nullable = false)
     private String name;
 
+    @Column(name = "create_at",nullable = false, updatable = false)
+    @CreationTimestamp // tự động cập nhật
+    private LocalDateTime createAt;
+
+    @Column(name = "update_at",nullable = false)
+    @UpdateTimestamp
+     private LocalDateTime update;
+
+    @PrePersist
+    public void prepersist() {
+        System.out.println("trươc khi thêm vào database");
+    }
 }
